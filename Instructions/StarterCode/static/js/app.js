2f420ce3341457389923240246234f7d2c6d1951
bandipara <b.andipara@hotmail.com>
2020-03-06 16:15:37 -0500

    Table Data Loaded to HTML

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,4 +1,18 @@
 // from data.js
-var tableData = data;
+//var tableData = data;
 
 // YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+
+// Console.log the weather data from data.js
+console.log(data);
+
+data.forEach((weatherReport) => {
+    var row = tbody.append("tr");
+    Object.entries(weatherReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  
